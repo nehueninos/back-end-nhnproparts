0346684b3bf0f54df92e3f22f09a3a606f770af5
nehueninos <nehuen.inos25@gmail.com>
2025-12-30 10:03:20 -0300

    cambio en upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,35 +1,7 @@
 import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from 'cloudinary';
-
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-// ⛔ FRENAMOS EL ERROR SI FALTAN VARIABLES
-if (
-  !process.env.CLOUDINARY_CLOUD_NAME ||
-  !process.env.CLOUDINARY_API_KEY ||
-  !process.env.CLOUDINARY_API_SECRET
-) {
-  console.error('❌ Cloudinary env vars missing');
-}
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary.v2,
-  params: {
-    folder: 'products',
-    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
-  },
-});
 
 const upload = multer({
-  storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
-  },
+  storage: multer.memoryStorage(),
 });
 
 export default upload;
